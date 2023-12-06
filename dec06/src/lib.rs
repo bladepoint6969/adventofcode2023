32bb133c515c87ebddccadf43cafdf6e033c9d96
Bennett Buckley <ben.buckley@outlook.com>
2023-12-06 10:31:50 -0500

    Completed December 06

--- dec06/src/lib.rs
+++ dec06/src/lib.rs
@@ -1,7 +1,43 @@
-pub fn part1(_input: &str) {
-    todo!();
+fn get_nums(input: &str) -> Vec<usize> {
+    input.split(' ').filter_map(|s| s.parse().ok()).collect()
 }
 
-pub fn part2(_input: &str) {
-    todo!();
+fn get_big_num(input: &str) -> usize {
+    input
+        .chars()
+        .filter(char::is_ascii_digit)
+        .collect::<String>()
+        .parse()
+        .unwrap()
+}
+
+fn possible_wins(time: usize, distance: usize) -> usize {
+    (0..=time)
+        .map(|speed| speed * (time - speed))
+        .filter(|&travelled| travelled > distance)
+        .count()
+}
+
+pub fn part1(input: &str) {
+    let mut lines = input.lines();
+    let times = get_nums(lines.next().unwrap());
+    let distances = get_nums(lines.next().unwrap());
+
+    let result: usize = times
+        .into_iter()
+        .zip(distances)
+        .map(|(time, distance)| possible_wins(time, distance))
+        .product();
+
+    println!("{result}");
+}
+
+pub fn part2(input: &str) {
+    let mut lines = input.lines();
+    let time = get_big_num(lines.next().unwrap());
+    let distance = get_big_num(lines.next().unwrap());
+
+    let result = possible_wins(time, distance);
+
+    println!("{result}")
 }
